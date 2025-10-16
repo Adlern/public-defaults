92781be3262bb65dc5213d70c233009e5acc1719
Adler <14262758+Adlern@users.noreply.github.com>
2025-10-17 00:59:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* load CSS */
 (async () => {
-	const url = "https://raw.githubusercontent.com/Adlern/public-defaults/refs/heads/main/main.css";
+	const url = "https://cdn.jsdelivr.net/gh/Adlern/public-defaults/main.css";
 	try {
 		const res = await fetch(url, { cache: "no-cache", signal: AbortSignal.timeout(10_000) });
 		if (!res.ok) throw new Error(`HTTP ${res.status} – ${res.statusText}`);
