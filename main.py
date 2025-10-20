40215375c1825c1c28981508b786f3f26db063ff
Adlern <14262758+Adlern@users.noreply.github.com>
2025-10-20 02:24:59 +0200

    a

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
-
+import os
 
 app = FastAPI(title="Simple FastAPI Server", version="1.0.0")
 
-# Root endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to the FastAPI server!"}
 
+public_dir = os.path.abspath("public")
+
+app.mount("/", StaticFiles(directory=public_dir, html=True), name="public")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
